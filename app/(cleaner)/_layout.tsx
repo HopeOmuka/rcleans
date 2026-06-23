f627e9bfd18902cb234915161b36428276f585bc
Hope <hope@rcleans.app>
2026-06-23 03:13:13 +0300

    feat(cleaner): implement job acceptance, availability update, and job management APIs
    - Added POST endpoint for accepting jobs, updating job status, and sending notifications.
    - Implemented availability update for cleaners.
    - Created GET endpoint for fetching available jobs and cleaner's jobs.
    - Developed cleaner login functionality with token generation.
    - Added UI components for cleaner dashboard, job management, and profile settings.
    - Integrated secure storage for cleaner session management.

---
+++ app/(cleaner)/_layout.tsx
@@ -0,0 +1,85 @@
+import { Tabs } from "expo-router";
+import { Image, ImageSourcePropType, View } from "react-native";
+
+import { icons } from "@/constants";
+
+const TabIcon = ({
+  source,
+  focused,
+}: {
+  source: ImageSourcePropType;
+  focused: boolean;
+}) => (
+  <View
+    className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-primary-500" : ""}`}
+  >
+    <View
+      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-primary-600" : ""}`}
+    >
+      <Image
+        source={source}
+        tintColor="white"
+        resizeMode="contain"
+        className="w-6 h-6"
+      />
+    </View>
+  </View>
+);
+
+export default function Layout() {
+  return (
+    <Tabs
+      initialRouteName="dashboard"
+      screenOptions={{
+        tabBarActiveTintColor: "#4ADE80",
+        tabBarInactiveTintColor: "#666",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#1F1F1F",
+          borderRadius: 50,
+          paddingBottom: 0,
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          position: "absolute",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="dashboard"
+        options={{
+          title: "Jobs",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.list} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="jobs"
+        options={{
+          title: "My Jobs",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.checkmark} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.profile} focused={focused} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
